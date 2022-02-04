f3c3fd274b71513c792096470a1f74423fabc2e5
Olga Pochenkova <amikkaby@gmail.com>
2022-02-05 00:38:43 +0300

    feat: implement the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const nAbs = Math.abs(n);
+  const array = nAbs.toString().split('');
+  const reverseArray = array.reverse();
+  const reverseStr = reverseArray.join('');
+  const reverseNum = parseInt(reverseStr);
+  return reverseNum;
 }
